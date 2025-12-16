a353c7c35a590ef33e29b00ca006de3582c61f32
Priya-gawhane <priyagawhane75@gmail.com>
2025-12-17 00:06:08 +0530

    nested function

--- function/06_scopes.py
+++ function/06_scopes.py
@@ -5,3 +5,16 @@ def serve_chai():
 chai_type = "badam chai"
 serve_chai()
 print(f"outside func{chai_type}")
+
+def chai_counter():
+    chai_order = 'lemon' #enclosing scope
+
+    def print_order():    #nested function
+        chai_order= 'ginger'
+        print("inner", chai_order)
+    print_order()
+    print("outer", chai_order)
+
+chai_order = 'tulsi' #global scope
+chai_counter()
+print("global", chai_order)
