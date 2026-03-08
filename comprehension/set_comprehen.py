18738bcb65b6f6bf2b2cd0acf53e2fc803e6cf5b
Priya-gawhane <priyagawhane75@gmail.com>
2026-03-08 23:47:58 +0530

    updated set comprehen

--- comprehension/set_comprehen.py
+++ comprehension/set_comprehen.py
@@ -8,3 +8,12 @@ u = {chai for chai in favourate}
 print(u)
 print(unique)
 
+RECIPES = {
+    "masala chai": ["tea leaves", "milk", "sugar", "spices"],
+    "lemon tea": ["tea leaves", "lemon", "sugar"],
+    "green tea": ["green tea leaves", "water"],
+    "elaichi chai": ["tea leaves", "milk", "sugar", "elaichi"]
+}
+
+unique_spices = {spice for ingredients in RECIPES.values() for spice in ingredients}
+print(unique_spices)
